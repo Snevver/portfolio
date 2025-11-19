e84ff2d94c1c86fff1f424341e08b9fb427d14a6
Penguin <son@vdburg.site>
2025-11-19 14:02:59 +0100

    Feat: New favicon

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -12,7 +12,7 @@
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
         <!-- Favicon -->
-        <link rel="shortcut icon" href="d-pad.png" type="image/x-icon">
+        <link rel="shortcut icon" href="logoWhite.png" type="image/x-icon">
 
         <!-- Scripts -->
         @routes
